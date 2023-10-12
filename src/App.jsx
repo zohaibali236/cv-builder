dfd776bdad505fa38699faa243bc25a8c7daf39e
Zohaib Ali <zohaib.ali236@gmail.com>
2023-10-12 23:18:20 +0500

    download resume

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-
 import PersonalDetails from './components/PersonalDetails';
 import EducationDetails from './components/EducationDetails';
 import ExperienceDetails from './components/ExperienceDetails';
 import Preview from './components/Preview';
 import Footer from './components/Footer';
+import html2canvas from 'html2canvas';
 
 function App() {
   const [personalInfo, setPersonalInfo] = useState({
@@ -25,7 +25,7 @@ function App() {
   console.log(educationInfo);
   return (
     <>
-      <div className='container m-auto mt-4 flex gap-x-20'>
+      <div className='md:container m-auto mt-4 flex gap-x-20'>
         <div className='flex flex-col gap-9'>
           <PersonalDetails
             details={personalInfo}
@@ -36,6 +36,33 @@ function App() {
             setEducationInfo={setEducationInfo}
           />
           <ExperienceDetails details={expInfo} setExpInfo={setExpInfo} />
+          <div className='flex justify-center'>
+            <button
+              className='flex items-center rounded-full bg-indigo-500 px-4 py-2 font-bold text-white hover:bg-indigo-600'
+              onClick={() => {const windowElement = document.getElementById('resume');
+
+              // Use html2canvas to capture the screenshot
+              html2canvas(windowElement).then(function (canvas) {
+                // Create a temporary link element
+                const link = document.createElement('a');
+                link.href = canvas.toDataURL('image/png');
+                link.download = 'screenshot.png';
+
+                // Programmatically click the link to trigger the download
+                link.click();
+              });
+}}
+            >
+              <svg
+                className='mr-2 h-4 w-4 fill-current'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+              >
+                <path d='M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z' />
+              </svg>
+              Download
+            </button>
+          </div>
         </div>
         <Preview
           personal={personalInfo}
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -8,7 +8,7 @@ import { Fragment } from 'react';
 
 export default function Preview({ personal, educational, expInfo }) {
   return (
-    <div className='container mr-4 max-h-[1200px] min-h-[1200px] max-w-[800px] border-2 text-white'>
+    <div id="resume" className='container mr-4 max-h-[1200px] min-h-[1200px] max-w-[800px] border-2 text-white'>
       <header className='flex h-40 flex-col items-center justify-center gap-y-4 bg-sky-600'>
         {personal.name !== '' && (
           <h1 className='text-3xl font-semibold '>{personal.name}</h1>
